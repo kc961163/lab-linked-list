8fb94343eb34df1be3251db9aa22484ab360e0a0
kc961163 <kc961163@gmail.com>
2023-01-24 15:27:57 -0500

    sll still working on it

--- linkedlist.c
+++ linkedlist.c
@@ -5,6 +5,35 @@
 #include <stdlib.h> // contains the functions free/malloc
 
 // TODO: Create your node_t type here
+typedef struct node {
+    int year;
+    int win;
+    stuct node* next;
+}node_t;
+
+node_t* newnode(int win, int year, node_t* next) {
+    node_t* newnode = (node_t*)malloc(sizeof(node_t));
+    if (newnode == NULL) return 0;
+    newnode->win = win;
+    newnode->year = year;
+    newnode->next = next;
+    return newnode;
+}
+
+void free_node(node_t* node){
+    if (node == NULL) return;
+    free(node);
+}
+
+void print(node_t* node) {
+    if (node == NULL) return;
+    node_t* iter = node;
+    while(iter != NULL){
+        printf("%d %d",iter->win, iter->year);
+        iter = iter->next;
+    }
+    printf("\n");
+}
 
 // TODO: Write your functions here
 // There should be 1.) create_list 2.) print_list 3.) free_list
@@ -16,6 +45,7 @@ int main()
     int test_wins[5] = {108, 93, 93, 78, 71};
 
     // TODO: Implement me!
+    node_t* head = newnode(10, 5, NULL);
 
     return 0;
 }
