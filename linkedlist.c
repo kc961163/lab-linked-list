86667eff8a0e4af4db9954dacaf3d4310d3d1cb3
kc961163 <kc961163@gmail.com>
2023-01-25 15:40:24 -0500

    Added changes to linkedlist now free, and build from array linked list is working.
    Work on insert and delete is left

--- linkedlist.c
+++ linkedlist.c
@@ -8,7 +8,7 @@
 typedef struct node {
     int year;
     int win;
-    stuct node* next;
+    struct node* next;
 }node_t;
 
 node_t* newnode(int win, int year, node_t* next) {
@@ -29,11 +29,16 @@ void print(node_t* node) {
     if (node == NULL) return;
     node_t* iter = node;
     while(iter != NULL){
-        printf("%d %d",iter->win, iter->year);
+        printf("%d %d ",iter->win, iter->year);
+        printf("\n");
         iter = iter->next;
     }
-    printf("\n");
+    
 }
+
+// node_t* build_list(int* wins, int* years, int size){
+     
+// }
 
 // TODO: Write your functions here
 // There should be 1.) create_list 2.) print_list 3.) free_list
@@ -45,7 +50,30 @@ int main()
     int test_wins[5] = {108, 93, 93, 78, 71};
 
     // TODO: Implement me!
-    node_t* head = newnode(10, 5, NULL);
+    // for(int i = 0; i < 5; i++){
+
+    // }
+
+    node_t* head = newnode(10,2005,NULL);
+    //node_t* node1 = newnode(20,2006, NULL);
+    //printf("wins %d year %d ", head->win, head->year);
+    //head->next = node1;
+    //printf("%d %d\n", head->next->win, head->next->year);
+    node_t* node1 = newnode(test_wins[0], test_years[0], NULL);
+    node_t* node2 = newnode(test_wins[1], test_years[1], NULL);
+    node_t* node3 = newnode(test_wins[2], test_years[2], NULL);
+    node_t* node4 = newnode(test_wins[3], test_years[3], NULL);
+    node_t* node5 = newnode(test_wins[4], test_years[4], NULL);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = NULL;
+
+    print(node1);
+
+
+
 
     return 0;
 }
